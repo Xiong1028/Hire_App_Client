0d9e57bed1902efea577f837ad80d84a93a3e702
xiong1028 <lixiong1124@gmail.com>
2019-01-11 17:09:23 -0800

    debugs all after git clone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import {HashRouter, Route, Switch} from "react-router-dom";
 import Register from './containers/register/register';
 import Login from './containers/login/login';
@@ -19,9 +18,5 @@ ReactDOM.render(
         <Route component={Main}></Route>
       </Switch>
     </HashRouter>
-  </Provider>, document.getElementById("root"));
-=======
-import {Button} from "react-bootstrap";
-
-ReactDOM.render(<Button className='col-12' bsStyle="success">Primary</Button>, document.getElementById("root"));
->>>>>>> 4b75af673064332ee411f4778e763c994995298b
+  </Provider>, document.getElementById("root")
+);
